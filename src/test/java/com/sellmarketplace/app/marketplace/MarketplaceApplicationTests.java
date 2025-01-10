44f8ebe1ca190e971c1327a80e112bfea199c650
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2025-01-10 08:35:16 -0500

    chore: init commit

---
+++ src/test/java/com/sellmarketplace/app/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellmarketplace.app.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
