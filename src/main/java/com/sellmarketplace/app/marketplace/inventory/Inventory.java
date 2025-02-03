fcaf2cf5a725ad53f1456dfd4547ea26f7724e46
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2025-02-03 14:18:19 -0500

    feat: create inventory

---
+++ src/main/java/com/sellmarketplace/app/marketplace/inventory/Inventory.java
@@ -0,0 +1,22 @@
+package com.sellmarketplace.app.marketplace.inventory;
+
+import com.sellmarketplace.app.marketplace.product.Product;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inventoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+}
