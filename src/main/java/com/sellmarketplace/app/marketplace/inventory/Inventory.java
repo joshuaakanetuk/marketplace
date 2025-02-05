9bfaf3b5e3218b09a65fababe083cdcb04bec58a
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2025-02-04 23:21:41 -0500

    feat: create inventory repo

--- src/main/java/com/sellmarketplace/app/marketplace/inventory/Inventory.java
+++ src/main/java/com/sellmarketplace/app/marketplace/inventory/Inventory.java
@@ -19,4 +19,6 @@ public class Inventory {
     @Column(nullable = false)
     private Integer quantity;
 
+
+
 }
---
+++ src/main/java/com/sellmarketplace/app/marketplace/inventory/InventoryRepository.java
@@ -0,0 +1,6 @@
+package com.sellmarketplace.app.marketplace.inventory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository {
+}
