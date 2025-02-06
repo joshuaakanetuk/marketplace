61d476f4877e9f64e60c98e5f012a769039bf0b2
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2025-02-05 23:27:17 -0500

    feat: create inventory service

---
+++ src/main/java/com/sellmarketplace/app/marketplace/inventory/InventoryService.java
@@ -0,0 +1,10 @@
+package com.sellmarketplace.app.marketplace.inventory;
+
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public InventoryService(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+}
